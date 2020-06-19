a3529653c0afa24ac2a0cba591b5c7c802a8f2c1
ogunbanwo opeyemi <opeyemi655@gmail.com>
2020-06-19 02:33:37 +0100

    added a requirements file for easier deploying

--- okkbuild/main/models.py
+++ okkbuild/main/models.py
@@ -13,14 +13,22 @@ class user_info(models.Model):
     def __str__(self):
         return self.user_category
 
-
+roles = (
+         ('user','User')
+         ('hro','HRO')
+         ('pso','PSO')
+         ('hrm','HRM')
+         ('hos','HOS')
+         ('admin','Admin')
+         ('pm','PM')
+)
 class user_login(models.Model):
     login_id = models.CharField(max_length=45)
     first_name = models.CharField(max_length=45, null = True)
     last_name = models.CharField(max_length=45,null = True)
     email = models.CharField(max_length=100,default='test@okkgrl.com')
     department = models.CharField(max_length=45, null=True)
-    role = models.CharField(max_length=45,null = True)
+    role = models.CharField(max_length=6, choices=roles, default='green')
     password = models.CharField(max_length=200,default='test')
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
