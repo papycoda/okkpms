10b6aea570c6d4d9d8e681a78867b3ed18ec422e
ogunbanwo opeyemi <opeyemi655@gmail.com>
2020-07-03 10:15:03 +0100

    heroku first trial

--- okkbuild/okkbuild/settings.py
+++ okkbuild/okkbuild/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'okkbuild.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'okkdb',
+        'USER':'postgres',
+        'PASSWORD':'october13',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
